14c0c7c7909b8ff48d559b0f31ca522cc2059bc5
Elijah Seed-Arita <elijaharita@gmail.com>
2023-01-16 09:45:34 -1000

    fix: wrong crypto lib

--- cmd/publisher/model.go
+++ cmd/publisher/model.go
@@ -8,7 +8,7 @@ import (
 	"time"
 
 	"github.com/ipfs/go-cid"
-	"github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/multiformats/go-multiaddr"
 	"gorm.io/gorm"
